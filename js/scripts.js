c5b27add3e7d7afb1f5fe0bee0e945b750fa4050
jd <duverseaujohnny21@gmail.com>
2020-06-01 17:19:10 -0400

    update jquery function and add image
    co-authored-by: Nikolas Kososik <nikolas.kososik@gmail.com>

--- js/scripts.js
+++ js/scripts.js
@@ -1,16 +1,20 @@
-jQuery("h1").click(function() {
-  alert("This is a header.");
-});
 
-jQuery("p").click(function() {
-  alert("This is a paragraph.");
-});
+$(document).ready(function() {
 
-jQuery("img").click(function() {
-  alert("This is an image.");
-});
-
-jQuery("h1").click(function() {
-  alert("This is a header.");
-  alert("I told you, THIS IS A HEADER!");
+  $("h1").click(function() {
+    alert("This is a header.");
+  });
+  
+  $("p").click(function() {
+    alert("This is a paragraph.");
+  });
+  
+  $("img").click(function() {
+    alert("This is an image.");
+  });
+  
+  $("h1").click(function() {
+    alert("This is a header.");
+    alert("I told you, THIS IS A HEADER!");
+  });
 });
